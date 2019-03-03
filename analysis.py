60c61900b0242bfd5e3816201bfe5d86d7997506
Vladimir_Yaroslavtsev <yaroslavtsev1395@gmail.com>
2019-03-03 12:47:55 +0300

    Rename: - log -> log_file; - ip_list -> user_list. Fixed docstrings.

--- analysis.py
+++ analysis.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 """ Web server log analysis."""
 
@@ -16,52 +15,49 @@ def read_log():
         return reader.read()
 
 
-def parse_log(log):
+def parse_log(log_file):
     """
-    Parse log file
-    :param log: result of read_log()
+    Parsing log file
+    :param log_file: result of read_log()
     :return: re.iterator with 2 groups: user and platform
     """
+
     reg_log = re.finditer(r'(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
                           r' - - \[\d\d/\w{3,10}/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4}\])'
-                          r'.+(\"\w{1,35}/\d+\.\d+ \(.+\))', log)
+                          r'.+(\"\w{1,35}/\d+\.\d+ \(.+\))', log_file)
     return reg_log
 
 
 def get_user(reg):
     """
     Get 10 user with the most requests
+    :param reg: result of parse_log(log_file)
     :return: list with nested tuple (user, quantity)
     """
 
-    ip_list = [user.group(1) for user in reg]
-    counter_user = Counter(ip_list).most_common(10)
-
+    user_list = [user.group(1) for user in reg]
+    counter_user = Counter(user_list).most_common(10)
     return counter_user
 
 
 def get_platform(reg):
     """
     Get 5 the most popular platform
+    :param reg: result of parse_log(log_file)
     :return: list with nested tuple (platform, quantity)
     """
 
     platform_list = [plat.group(2) for plat in reg]
     counter_platform = Counter(platform_list).most_common(5)
-
     return counter_platform
 
 
 def main():
-    """
-    main()
-    :return: None
-    """
+    log = read_log()
+    parse = list(parse_log(log))
     print get_user(parse), '\n'
     print get_platform(parse)
 
 
 if __name__ == '__main__':
-    log = read_log()
-    parse = list(parse_log(log))
     main()
